a3ddeb2a18d6186985669e581e64a369cab432ac
ABC43420CBA ABC43420cba <1657870178@QQ.com>
2023-06-25 04:35:18 +0000

    ?

--- my_sqrt.py
+++ my_sqrt.py
@@ -1,4 +1,4 @@
-
+'''
 S = 10
 infinitesimal = 0.000001
 
@@ -24,24 +24,25 @@ a = (a + b) / 2
 b = S / a
 delta = a - b
 print(f'这是第4轮，S={S}, a={a}, b={b}, delta={delta}, 是否结束：{delta < infinitesimal}')
-
+'''
 
 print('下面我们用循环来重新计算这个问题')
 
-S = 10
-infinitesimal = 0.00000000000000000000000000000000000001
+for S in range(1,101):
 
-a = S / 2
+    infinitesimal = 0.000001
 
-counter = 1
-while True:
-    b = S / a
-    delta = a - b
-    print(f'这是第{counter}轮，S={S}, a={a}, b={b}, delta={delta}, 是否结束：{delta < infinitesimal}')
+    a = S / 2
+
+    counter = 1
+    while True:
+        b = S / a
+        delta = a - b
+        print(f'这是第{counter}轮，S={S}, a={a}, b={b}, delta={delta}, 是否结束：{delta < infinitesimal}')
     
-    counter = counter + 1
+        counter = counter + 1
 
-    if delta < infinitesimal:
-        break
-    else:
-        a = a = (a + b) / 2
+        if delta < infinitesimal:
+            break
+        else:
+            a = a = (a + b) / 2
